b379d43a5a329dd776c87c20591efc3a45aaaf29
Olivier VALLOD <olivier.vallod@fr.ibm.com>
2023-09-01 20:44:52 +0200

    Update app.py vers 3
    version 3

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World ! - OV Demo - New version</h1>'
+    return '<h1>Hello, World ! - OV Demo - New version 3.0</h1>'
 
 @app.route('/version')
 def get_version():
-    return '<h1>App version : <b>2.1</b></h1>'
+    return '<h1>App version : <b>3.0</b></h1>'
 
 @app.route('/test')
 def get_test():
